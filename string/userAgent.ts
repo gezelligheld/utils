0474f5bb93aed3830e75ea913770976c41535b85
maojiaqing <maojiaqing@xiaoice.com>
2021-09-29 10:51:56 +0800

    ua

---
+++ string/userAgent.ts
@@ -0,0 +1,10 @@
+// 是否是移动端
+export function isMobile() {
+    return /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)
+        && typeof window.ontouchstart !== 'undefined';
+}
+
+// 是否是微信浏览器
+export function isWx() {
+    return /MicroMessenger/.test(navigator.userAgent);
+}
