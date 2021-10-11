03d77d75fa82c4acd838aa0996bf8512c0266d54
maojiaqing <maojiaqing@xiaoice.com>
2021-10-11 14:11:48 +0800

    fix

--- js/eventEmitter.ts
+++ js/eventEmitter.ts
@@ -1,12 +1,12 @@
 export default class EventEmitter {
-    events: {[evt: string]: any[] | null} = {};
+    events: { [evt: string]: any[] | null } = {};
 
     on = (evt: string, cb: (...args: any[]) => void) => {
         if (!this.events[evt]) {
             this.events[evt] = [];
         }
         this.events[evt]!.push(cb);
-    }
+    };
 
     off = (evt: string, cb?: (...args: any[]) => void) => {
         if (cb) {
@@ -15,7 +15,7 @@ export default class EventEmitter {
         else {
             this.events[evt] = null;
         }
-    }
+    };
 
     // 只触发一次
     once = (evt: string, cb: (...args: any[]) => void) => {
@@ -24,7 +24,7 @@ export default class EventEmitter {
             cb.apply(this, args);
         };
         this.on(evt, func);
-    }
+    };
 
     emit = (evt: string, ...args: any[]) => {
         const cbs = this.events[evt];
@@ -33,5 +33,5 @@ export default class EventEmitter {
                 cb.apply(this, args);
             });
         }
-    }
+    };
 }
--- react/hooks/useDrag.ts
+++ react/hooks/useDrag.ts
@@ -1,97 +1,92 @@
-/**
- * @file components/common/CropUpload/hooks/dragUpload 拖拽上传
- * @author maojiaqing
- */
+import _ from 'lodash';
+import { useEffect, useCallback, useState } from 'react';
 
- import _ from 'lodash';
- import {useEffect, useCallback, useState} from 'react';
+import getSuffixByName from 'string/getSuffixName';
 
- import getSuffixByName from 'string/getSuffixName';
-
- interface DragProps {
+interface DragProps {
     fileNum: number,
     accept: string;
     classNamePattern: RegExp;
-    onChange: (param: {file: File, fileList: File[]}) => void,
- }
+    onChange: (param: { file: File, fileList: File[] }) => void,
+}
  
- export default (props: DragProps) => {
-     const {
-         // 文件数量
-         fileNum,
-         // 文件格式
-         accept,
-         // 目标区域类名正则
-         classNamePattern,
-         // 拖拽释放的回调
-         onChange
-     } = props;
+export default (props: DragProps) => {
+    const {
+        // 文件数量
+        fileNum,
+        // 文件格式
+        accept,
+        // 目标区域类名正则
+        classNamePattern,
+        // 拖拽释放的回调
+        onChange,
+    } = props;
  
-     const [fileList, setFileList] = useState();
-     const [dragging, setDragging] = useState(false);
+    const [fileList, setFileList] = useState();
+    const [dragging, setDragging] = useState(false);
  
-     const handleDrop = useCallback((e: any) => {
-         const files = e.dataTransfer.files;
-         const fileList = _.values(_.omit(files, ['length', 'item']));
-         if (fileList.length > fileNum) {
-             return;
-         }
-         if (accept) {
-             const types = _.map(accept.split(','), (type: string) => _.replace(type, /^\./, ''));
-             let flag = true;
-             _.each(fileList, (file: File) => {
-                 if (types.indexOf(getSuffixByName(file.name)) < 0) {
-                     flag = false;
-                 }
-             });
-             if (!flag) {
-                 return;
-             }
-         }
-         onChange && onChange({file: _.get(files, '0'), fileList});
-         setFileList(files);
-     }, [fileNum, accept, onChange]);
+    const handleDrop = useCallback((e: any) => {
+        const { files } = e.dataTransfer;
+        const fileList = _.values(_.omit(files, ['length', 'item']));
+        if (fileList.length > fileNum) {
+            return;
+        }
+        if (accept) {
+            const types = _.map(accept.split(','), (type: string) => _.replace(type, /^\./, ''));
+            let flag = true;
+            _.each(fileList, (file: File) => {
+                if (types.indexOf(getSuffixByName(file.name)) < 0) {
+                    flag = false;
+                }
+            });
+            if (!flag) {
+                return;
+            }
+        }
+        onChange && onChange({ file: _.get(files, '0'), fileList });
+        setFileList(files);
+    }, [fileNum, accept, onChange]);
  
-     const handleFileDrag = useCallback((e: any) => {
-         e.preventDefault();
-         e.stopPropagation();
-         if (classNamePattern.test(_.get(e, 'target.className'))) {
-             switch (e.type) {
-                 case 'dragenter':
-                     setDragging(true);
-                     break;
-                 case 'dragover':
-                     e.dataTransfer.dropEffect = 'copy';
-                     break;
-                 case 'drop':
+    const handleFileDrag = useCallback((e: any) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (classNamePattern.test(_.get(e, 'target.className'))) {
+            switch (e.type) {
+                case 'dragenter':
+                    setDragging(true);
+                break;
+                case 'dragover':
+                    e.dataTransfer.dropEffect = 'copy';
+                    break;
+                case 'drop':
+                setDragging(false);
+                    handleDrop(e);
+                    break;
+                case 'dragleave':
                     setDragging(false);
-                     handleDrop(e);
-                     break;
-                 case 'dragleave':
-                    setDragging(false);
-                     break;
-                 default:
-                     break;
-             }
-         }
-     }, [classNamePattern, handleDrop]);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }, [classNamePattern, handleDrop]);
  
-     useEffect(() => {
-         document.addEventListener('dragenter', handleFileDrag);
-         document.addEventListener('dragover', handleFileDrag);
-         document.addEventListener('drop', handleFileDrag);
-         document.addEventListener('dragleave', handleFileDrag);
+    useEffect(() => {
+        document.addEventListener('dragenter', handleFileDrag);
+        document.addEventListener('dragover', handleFileDrag);
+        document.addEventListener('drop', handleFileDrag);
+        document.addEventListener('dragleave', handleFileDrag);
  
-         return () => {
-             document.removeEventListener('dragover', handleFileDrag);
-             document.removeEventListener('drop', handleFileDrag);
-             document.removeEventListener('dragleave', handleFileDrag);
-             document.removeEventListener('dragenter', handleFileDrag);
-         };
-     }, []);
+        return () => {
+            document.removeEventListener('dragover', handleFileDrag);
+            document.removeEventListener('drop', handleFileDrag);
+            document.removeEventListener('dragleave', handleFileDrag);
+            document.removeEventListener('dragenter', handleFileDrag);
+        };
+    }, []);
  
-     return {
-         fileList,
-         dragging
-     };
- };
+    return {
+        fileList,
+        dragging,
+    };
+};
